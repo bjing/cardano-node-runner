1b37a1ec5f8460f14c672fc793b2a1ba7d66a0ff
Brian Jing <brian.jing@tabcorp.com.au>
2023-09-01 15:52:27 +1000

    Fix query-cardano-tip.sh

--- scripts/query-cardano-tip.sh
+++ scripts/query-cardano-tip.sh
@@ -5,10 +5,10 @@ source ~/.bash_cardano
 NETWORK=$1
 if [ "$NETWORK" = "preview" ]; then
   CARDANO_NODE_PATH=$CARDANO_NODE_PREVIEW
-  NET_MAGIC="--testnet-magic 1"
+  NET_MAGIC="--testnet-magic 2"
 elif [ "$NETWORK" = "preprod" ]; then
   CARDANO_NODE_PATH=$CARDANO_NODE_PREPROD
-  NET_MAGIC="--testnet-magic 2"
+  NET_MAGIC="--testnet-magic 1"
 elif [ "$NETWORK" = "mainnet" ]; then
   CARDANO_NODE_PATH=$CARDANO_NODE_MAINNET
   NET_MAGIC="--mainnet"
@@ -19,6 +19,5 @@ fi
 
 SOCKET_PATH="$CARDANO_NODE_PATH/runtime/local/ipc/node.socket"
 
-cardano-cli query tip \
-          --socket-path "$SOCKET_PATH" \
-          "$NET_MAGIC"
+CMD="cardano-cli query tip --socket-path $SOCKET_PATH $NET_MAGIC"
+eval $CMD
